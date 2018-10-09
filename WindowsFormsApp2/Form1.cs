ee84f7d38222ba7e23ec3f15a939488d498f32fe
volotkov <evroandrew@gmail.com>
2018-10-09 20:17:19 +0300

    tsk

--- WindowsFormsApp2/Form1.cs
+++ WindowsFormsApp2/Form1.cs
@@ -1,20 +1,57 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace WindowsFormsApp2
 {
-    public partial class Form1 : Form
+    public partial class MyForm : Form
     {
-        public Form1()
+        bool enableClosing=false;
+        MyLock @lock = new MyLock("123#");
+        int time = 1; Process p;
+        public MyForm()
         {
+            @lock.Unlock += lock_Unlock;
+            TopMost = true;
             InitializeComponent();
+            this.Load += new EventHandler(Form1_Load);
+        }
+        private void lock_Unlock()
+        {
+            enableClosing = true;
+            Close();
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            Button b = sender as Button;
+            if (@lock.Check(b.Text[0]) == -1)
+                Thread.Sleep(time*100);
+            time++;
+        }
+        void Form1_Load(object sender, EventArgs e)
+        {
+            p = new Process();
+            p.StartInfo.WorkingDirectory = Environment.GetFolderPath(Environment.SpecialFolder.System);
+            p.StartInfo.FileName = "taskmgr.exe";
+            p.StartInfo.CreateNoWindow = true;
+            p.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            p.Start();
+            this.Focus();
+        }
+        private void MyForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (enableClosing) return;
+            Process.GetProcessesByName("taskmgr")[0].Kill();
+            e.Cancel = true;
         }
     }
 }
---
+++ WindowsFormsApp2/MyLock.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+namespace WindowsFormsApp2
+{
+    delegate void UnlockDelegate();
+
+    internal class MyLock
+    {
+        char[] codeArr;
+        List<char> input_stream = new List<char>();
+        public MyLock(string code)
+        {
+            codeArr = code.ToCharArray();
+        }
+        public event UnlockDelegate Unlock;
+        public int Check(char code)
+        {
+            int check = 0;
+            input_stream.Add(code);
+            if (input_stream.Count > codeArr.Length)
+                input_stream.RemoveAt(0);
+            if (input_stream.Count == codeArr.Length)
+            {
+                for (int i = input_stream.Count - 1; i > 0; i--)
+                {
+                    if (input_stream[i] != codeArr[i])
+                    {
+                        check = -1;
+                        break;
+                    }
+                }
+                if (check == 0)
+                    Unlock?.Invoke();
+            }
+            return check;
+        }
+    }
+}
